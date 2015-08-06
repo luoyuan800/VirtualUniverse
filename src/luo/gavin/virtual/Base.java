8ec2345883d403184ef9d7886916efa9d1e96ff0
Gavin <Gavin>
2015-08-06 08:51:49 +0800

    Inside

--- src/luo/gavin/virtual/Base.java
+++ src/luo/gavin/virtual/Base.java
@@ -5,7 +5,7 @@ import luo.gavin.output.Output;
 
 import java.util.Random;
 
-public abstract class Base {
+public abstract class Base implements  Runnable{
     protected double stardust;
     private long age;
     protected int count;
@@ -13,11 +13,14 @@ public abstract class Base {
     private int y;
     private int radii;
     protected Output output;
-    protected void LOG(String msg){
+    protected boolean die;
+
+    protected void LOG(String msg) {
         output.LOG(msg);
     }
 
     public abstract Random getRandom();
+
     public Base(int x, int y, int radii) {
         this.x = x;
         this.y = y;
@@ -25,11 +28,12 @@ public abstract class Base {
         this.output = new Output(this.getClass().getSimpleName() + x + "_" + y);
     }
 
-    public void start(){
+    public void start() {
         LOG(String.format("Point: x=%s, y=%s; Radii: %s; Stardust: %s", x, y, radii, stardust));
         LOG("------");
 
     }
+
     public void age() {
         count++;
         if (count == 100) {
@@ -52,7 +56,7 @@ public abstract class Base {
     }
 
     public boolean isInside(int x, int y) {
-        return MathUtils.square(x - getX()) + MathUtils.square(y - getY()) <= MathUtils.square(getRadii());
+        return Math.hypot(x-getX(), y-getY()) <= getRadii();
 //        return ((this.getX() + this.getRadii() >= x && this.getX() - getRadii() <= x) && (this.getY() + this.getRadii() >= y && this.getY() - this.getRadii() <= y));
     }
 
@@ -86,23 +90,24 @@ public abstract class Base {
     public static final int CONTAIN = 1;
     public static final int CONTAINED = 2;
 
-    public int[] getRandomPoint(){
+    public int[] getRandomPoint() {
         int x = this.getX() + getRandom().nextInt(this.getRadii()) * (getRandom().nextBoolean() ? -1 : 1);
         int y = this.getY() + getRandom().nextInt(this.getRadii()) * (getRandom().nextBoolean() ? -1 : 1);
         return new int[]{x, y};
     }
 
-    protected int getRandomRadii(){
-        int radii = (int)(getRandom().nextInt(this.radii) * getRandom().nextFloat());
-        if(radii<=0) return 1;
+    protected int getRandomRadii() {
+        int radii = (int) (getRandom().nextInt(this.radii) * getRandom().nextFloat());
+        if (radii <= 0) return 1;
         return radii;
     }
 
-    public String toString(){
+    public String toString() {
         return String.format("%s x=%s, y=%s, stardust=%s", this.getClass().getSimpleName(), x, y, stardust);
     }
 
-    public void stop(){
+    public void stop() {
+        die = true;
         LOG("Finished stop " + toString());
     }
 
@@ -114,7 +119,40 @@ public abstract class Base {
         this.y = y;
     }
 
-    public boolean hit(Base other){
-        return false;
+    public boolean hit(Base other) {
+        boolean hit = false;
+        if (this.isTouch(other)) {
+            if (this.radii > 2 * other.getRadii()) {
+                this.stardust += other.stardust;
+                LOG("A " + other.getClass().getSimpleName() + " hit this " + getClass().getSimpleName());
+                LOG("Hitter is " + other);
+            } else if(other.getRadii() > 2 * this.radii){
+                LOG("This " + getClass().getSimpleName() + " hit a " + other.getClass().getSimpleName());
+                LOG("Target is " + other);
+                hit = true;
+                this.stop();
+            } else{
+                LOG("Die for the hit a " + other.getClass().getSimpleName());
+                LOG("The target is " + other);
+                hit = true;
+                this.stop();
+            }
+        }
+        return hit;
     }
+
+    public boolean isDie() {
+        return die;
+    }
+
+    public void run(){
+        try{
+            doRun();
+        }catch (Exception e){
+            LOG(e.toString());
+            e.printStackTrace();
+        }
+    }
+
+    protected abstract void doRun();
 }
